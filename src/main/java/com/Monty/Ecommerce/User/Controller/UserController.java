67fa208d723682b954f7cbfe598181222dec78b2
FakihAli439 <72967368+FakihAli439@users.noreply.github.com>
2021-05-25 09:22:37 +0300

    update

--- src/main/java/com/Monty/Ecommerce/User/Controller/UserController.java
+++ src/main/java/com/Monty/Ecommerce/User/Controller/UserController.java
@@ -22,17 +22,13 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  *
  * @author Admin
  */
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
--- src/main/java/com/Monty/Ecommerce/Vendor/Controller/VendorController.java
+++ src/main/java/com/Monty/Ecommerce/Vendor/Controller/VendorController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class VendorController {
